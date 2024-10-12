b957a99aa607975f688c26647907115061bc1e7e
feras12345 <53536761+feras12345@users.noreply.github.com>
2024-10-13 00:57:33 +0300

    Update teeest.cpp

--- pointer/5-heap/heap-function/malloc-heap/dynamic array/callor-realloc/teeest.cpp
+++ pointer/5-heap/heap-function/malloc-heap/dynamic array/callor-realloc/teeest.cpp
@@ -10,12 +10,11 @@ int main()
 	int n, i;
 
 	// Get the number of elements for the array
-	printf("Enter number of elements:");
-	scanf_s("%d", &n);
-	printf("Entered number of elements: %d\n", n);
+	n = 5;
+	printf("Enter number of elements: %d\n", n);
 
-	// Dynamically allocate memory using malloc()
-	ptr = (int*)malloc(n * sizeof(int));
+	// Dynamically allocate memory using calloc()
+	ptr = (int*)calloc(n, sizeof(int));
 
 	// Check if the memory has been successfully
 	// allocated by malloc or not
@@ -26,7 +25,7 @@ int main()
 	else {
 
 		// Memory has been successfully allocated
-		printf("Memory successfully allocated using malloc.\n");
+		printf("Memory successfully allocated using calloc.\n");
 
 		// Get the elements of the array
 		for (i = 0; i < n; ++i) {
@@ -38,9 +37,35 @@ int main()
 		for (i = 0; i < n; ++i) {
 			printf("%d, ", ptr[i]);
 		}
-	}
 
-	
+		// Get the new size for the array
+		n = 10;
+		printf("\n\nEnter the new size of the array: %d\n", n);
+
+		// Dynamically re-allocate memory using realloc()
+		ptr = (int*)realloc(ptr, n * sizeof(int));
+
+		if (ptr == NULL) {
+			printf("Reallocation Failed\n");
+			exit(0);
+		}
+
+		// Memory has been successfully allocated
+		printf("Memory successfully re-allocated using realloc.\n");
+
+		// Get the new elements of the array
+		for (i = 5; i < n; ++i) {
+			ptr[i] = i + 1;
+		}
+
+		// Print the elements of the array
+		printf("The elements of the array are: ");
+		for (i = 0; i < n; ++i) {
+			printf("%d, ", ptr[i]);
+		}
+
+		free(ptr);
+	}
 
 	return 0;
 }
@@ -53,6 +78,5 @@ int main()
 	}
 	*/
 	
-
 
 
